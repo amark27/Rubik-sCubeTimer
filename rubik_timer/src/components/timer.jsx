051d57429fb6a1cc851704b66aab1331f438208a
Alex Mark <alex.mark@mail.utoronto.ca>
2019-07-03 22:37:34 -0400

    Fixed error with displaying the timer

--- rubik_timer/src/components/timer.jsx
+++ rubik_timer/src/components/timer.jsx
@@ -1,17 +1,36 @@
 import React, { Component } from "react";
 
 class Timer extends Component {
-  state = { time: 0, running: false };
+ 
+  constructor(){
+    super();
+    this.state = { time: 0, running: false };
+  }
+
+  componentDidMount(){
+    window.addEventListener('keypress', this.incrementOrStop);
+  }
+
+  incrementOrStop = () => {
+    return this.state.running ? this.stop : this.increment;
+  }
 
   increment = () => {
-    this.setState({ time: time++, running: true });
+    if (!this.state.running)
+      this.timerID = setInterval(() => this.setState({ time: this.state.time + 1, running: true }), 100);
   };
+
+  stop = () => {
+    clearInterval(this.timerID);
+    this.setState({time: this.state.time, running: false});
+  }
+
   render() {
     return (
       <React.Fragment>
         <h1>{this.state.time}</h1>
         <button onClick={this.increment}>Start</button>
-        <button>Stop</button>
+        <button onClick={this.stop}>Stop</button>
         <button>Reset</button>
       </React.Fragment>
     );
