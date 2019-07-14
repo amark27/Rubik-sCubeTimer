671ffbc7322d9a8e631fea0b3e73192ad2387844
Alex Mark <alex.mark@mail.utoronto.ca>
2019-07-13 22:20:48 -0400

    timer fully functional with space bar

--- rubik_timer/src/App.js
+++ rubik_timer/src/App.js
@@ -3,9 +3,24 @@ import logo from "./logo.svg";
 import "./App.css";
 import Timer from "./components/timer.jsx";
 
+const SPACE_KEY = 32;
+
 class App extends Component {
+  state = {
+    timerRunning: false
+  };
+
+  componentDidMount(){
+    window.addEventListener('keyup', (e) => this.updateTimer(e));
+  }
+
+  updateTimer = (e) => {
+    if (e.keyCode === SPACE_KEY)
+      this.setState((state) => ({timerRunning: !state.timerRunning}))
+  }
+
   render() {
-    return <Timer />;
+    return <Timer running={this.state.timerRunning}/>;
   }
 }
 
--- rubik_timer/src/components/timer.jsx
+++ rubik_timer/src/components/timer.jsx
@@ -1,40 +1,57 @@
 import React, { Component } from "react";
+import "../scss-css/styles.css";
 
 class Timer extends Component {
- 
-  constructor(){
-    super();
-    this.state = { time: 0, running: false };
-  }
+	constructor(props) {
+		super(props);
+		this.state = { min: 0, sec: 0, msec: 0, running: this.props.running };
+	}
 
-  componentDidMount(){
-    window.addEventListener('keypress', this.incrementOrStop);
-  }
+	componentDidUpdate(prevProps) {
+		if (prevProps.running !== this.props.running) {
+      if (prevProps.running) 
+        this.stop();
+      else {
+        this.setState({min: 0, sec: 0, msec: 0});
+        this.increment();
+      }
+		}
+	}
 
-  incrementOrStop = () => {
-    return this.state.running ? this.stop : this.increment;
-  }
+	increment = () => {
+    this.timerID = setInterval(this.incrementHelper, 10);
+	};
 
-  increment = () => {
-    if (!this.state.running)
-      this.timerID = setInterval(() => this.setState({ time: this.state.time + 1, running: true }), 100);
-  };
+	incrementHelper = () => {
+    if (this.state.msec < 99)
+      this.setState(state => ({ msec: state.msec + 1 }));
+    else if (this.state.sec < 59)
+      this.setState(state => ({ sec: state.sec + 1, msec: 0 }));
+    else
+      this.setState(state => ({ min: state.min + 1, sec: 0, msec: 0 }));
 
-  stop = () => {
-    clearInterval(this.timerID);
-    this.setState({time: this.state.time, running: false});
-  }
+	};
 
-  render() {
-    return (
-      <React.Fragment>
-        <h1>{this.state.time}</h1>
-        <button onClick={this.increment}>Start</button>
-        <button onClick={this.stop}>Stop</button>
-        <button>Reset</button>
-      </React.Fragment>
-    );
+	stop = () => {
+		clearInterval(this.timerID);
+		this.setState({ running: false });
+	};
+
+  formatNumber = (num) => {
+    return ("0" + num).slice(-2);
   }
+  
+	render() {    
+    let min = this.state.min != 0 ? this.state.min : null;
+    let sec = this.state.min != 0 ? this.formatNumber(this.state.sec) : this.state.sec;
+    let msec = this.formatNumber(this.state.msec);
+
+		return (
+			<React.Fragment>
+        <h1 className="timer">{!!(min) ? this.state.min + ":" : ""}{sec}.{msec}</h1>
+			</React.Fragment>
+		);
+	}
 }
 
 export default Timer;
