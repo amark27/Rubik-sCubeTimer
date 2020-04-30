ea2ca8c0cdbc198a718b2e0c45a4ec3c2ee74e8e
Alex Mark <alex.mark@mail.utoronto.ca>
2020-04-30 19:59:22 -0400

    changed css of table

--- rubik_timer/src/components/timeTable.jsx
+++ rubik_timer/src/components/timeTable.jsx
@@ -11,24 +11,26 @@ class TableTimes extends Component {
 	render() {
 		let { times } = this.props;
 		return (
-			<div id="tableTimes" className="container-table">
-				<table className="timeTable">
-					<tbody>
-						<tr>
-							<th>ID</th>
-							<th>Time</th>
-						</tr>
-						{times.map((curr, index) => {
-							let { min, sec, msec } = separateSec(curr);
-							return (
-								<tr key={index}>
-									<td>{index + 1}</td>
-									<td>{displayTime(min, sec, msec)}</td>
-								</tr>
-							);
-						})}
-					</tbody>
-				</table>
+			<div className="container-scroll">
+				<div id="tableTimes" className="container-table">
+					<table className="timeTable">
+						<tbody>
+							<tr>
+								<th>ID</th>
+								<th>Time</th>
+							</tr>
+							{times.map((curr, index) => {
+								let { min, sec, msec } = separateSec(curr);
+								return (
+									<tr key={index}>
+										<td>{index + 1}</td>
+										<td>{displayTime(min, sec, msec)}</td>
+									</tr>
+								);
+							})}
+						</tbody>
+					</table>
+				</div>
 			</div>
 		);
 	}
